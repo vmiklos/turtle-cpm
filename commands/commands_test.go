435d0bfed8d3a8d17e3918161f5a5fecce93f476
Miklos Vajna <vmiklos@vmiklos.hu>
2022-07-31 15:30:13 +0200

    Extract delete_test.go from commands_test.go

--- commands/commands_test.go
+++ commands/commands_test.go
@@ -49,51 +49,6 @@ func ContainsString(items []string, item string) bool {
 	return false
 }
 
-func TestDelete(t *testing.T) {
-	db, err := CreateDatabaseForTesting()
-	defer db.Close()
-	if err != nil {
-		t.Fatalf("CreateDatabaseForTesting() err = %q, want nil", err)
-	}
-	OldOpenDatabase := OpenDatabase
-	OpenDatabase = OpenDatabaseForTesting(db)
-	defer func() { OpenDatabase = OldOpenDatabase }()
-	OldCloseDatabase := CloseDatabase
-	CloseDatabase = CloseDatabaseForTesting
-	defer func() { CloseDatabase = OldCloseDatabase }()
-	expectedMachine := "mymachine"
-	expectedService := "myservice"
-	expectedUser := "myuser"
-	expectedPassword := "mypassword"
-	expectedType := "plain"
-	err = initDatabase(db)
-	if err != nil {
-		t.Fatalf("initDatabase() = %q, want nil", err)
-	}
-	_, err = createPassword(db, expectedMachine, expectedService, expectedUser, expectedPassword, expectedType)
-	if err != nil {
-		t.Fatalf("createPassword() = %q, want nil", err)
-	}
-	os.Args = []string{"", "delete", "-m", expectedMachine, "-s", expectedService, "-u", expectedUser}
-	buf := new(bytes.Buffer)
-
-	actualRet := Main(buf)
-
-	expectedRet := 0
-	if actualRet != expectedRet {
-		t.Fatalf("Main() = %q, want %q", actualRet, expectedRet)
-	}
-	results, err := readPasswords(db, "", "", "", "", false, false, []string{})
-	if err != nil {
-		t.Fatalf("readPasswords() err = %q, want nil", err)
-	}
-	actualLength := len(results)
-	expectedLength := 0
-	if actualLength != expectedLength {
-		t.Fatalf("actualLength = %q, want %q", actualLength, expectedLength)
-	}
-}
-
 func copyPath(inPath, outPath string) error {
 	outFile, err := os.Create(outPath)
 	if err != nil {
