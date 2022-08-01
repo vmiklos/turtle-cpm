94afbd118c2917094a3b63413c88813f770b8732
Miklos Vajna <vmiklos@vmiklos.hu>
2022-08-01 17:40:44 +0200

    man: lock down the date of the pages
    Otherwise each manpage will be changed all the time.

--- man/generate.go
+++ man/generate.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"time"
 
 	"github.com/spf13/cobra/doc"
 	"vmiklos.hu/go/turtle-cpm/commands"
@@ -10,9 +11,11 @@ import (
 func main() {
 	var ctx commands.Context
 	cmd := commands.NewRootCommand(&ctx)
+	date := time.Date(2022, 7, 22, 12, 0, 0, 0, time.UTC)
 	header := &doc.GenManHeader{
 		Title:   "CPM",
 		Section: "1",
+		Date:    &date,
 	}
 	err := doc.GenManTree(cmd, header, "man")
 	if err != nil {
