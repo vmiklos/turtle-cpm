f092ce4b17600caac319194b46b0e96d2b41b6fa
Miklos Vajna <vmiklos@vmiklos.hu>
2023-02-24 21:53:40 +0100

    update: always select password using an ID
    cpm update -i ... -p ... now doesn't crash and specifying all of
    machine/service/user/type is just painful.

--- commands/update_test.go
+++ commands/update_test.go
@@ -13,12 +13,12 @@ func TestUpdate(t *testing.T) {
 	expectedService := "myservice"
 	expectedUser := "myuser"
 	expectedPassword := "newpassword"
-	var expectedType PasswordType = "plain"
+	var expectedType PasswordType = "newtype"
 	_, err := createPassword(&ctx, expectedMachine, expectedService, expectedUser, "oldpassword", expectedType)
 	if err != nil {
 		t.Fatalf("createPassword() = %q, want nil", err)
 	}
-	os.Args = []string{"", "update", "-m", expectedMachine, "-s", expectedService, "-u", expectedUser, "-p", expectedPassword}
+	os.Args = []string{"", "update", "-i", "1", "-p", expectedPassword}
 	inBuf := new(bytes.Buffer)
 	outBuf := new(bytes.Buffer)
 
@@ -62,7 +62,7 @@ func TestPwgenUpdate(t *testing.T) {
 	if err != nil {
 		t.Fatalf("createPassword() = %q, want nil", err)
 	}
-	os.Args = []string{"", "update", "-m", expectedMachine, "-s", expectedService, "-u", expectedUser}
+	os.Args = []string{"", "update", "-i", "1", "-p", "-"}
 	inBuf := new(bytes.Buffer)
 	outBuf := new(bytes.Buffer)
 
@@ -72,7 +72,7 @@ func TestPwgenUpdate(t *testing.T) {
 	if actualRet != expectedRet {
 		t.Fatalf("Main() = %q, want %q", actualRet, expectedRet)
 	}
-	expectedBuf := "Generated new password: output-from-pwgen\nUpdated 1 password\n"
+	expectedBuf := "Updated 1 password\nGenerated password: output-from-pwgen\n"
 	if outBuf.String() != expectedBuf {
 		t.Fatalf("Main() output is %q, want %q", outBuf.String(), expectedBuf)
 	}
@@ -105,9 +105,9 @@ func TestInteractiveUpdate(t *testing.T) {
 	if err != nil {
 		t.Fatalf("createPassword() = %q, want nil", err)
 	}
-	os.Args = []string{"", "update", "-s", expectedService, "-p", expectedPassword}
+	os.Args = []string{"", "update", "-p", expectedPassword}
 	inBuf := new(bytes.Buffer)
-	inBuf.Write([]byte(expectedMachine + "\n" + expectedUser + "\n"))
+	inBuf.Write([]byte("1\n"))
 	outBuf := new(bytes.Buffer)
 
 	actualRet := Main(inBuf, outBuf)
@@ -116,7 +116,7 @@ func TestInteractiveUpdate(t *testing.T) {
 	if actualRet != expectedRet {
 		t.Fatalf("Main() = %q, want %q", actualRet, expectedRet)
 	}
-	expectedBuf := "Machine: User: "
+	expectedBuf := "Id: "
 	expectedBuf += "Updated 1 password\n"
 	if outBuf.String() != expectedBuf {
 		t.Fatalf("Main() output is %q, want %q", outBuf.String(), expectedBuf)
@@ -149,8 +149,7 @@ func TestDryRunUpdate(t *testing.T) {
 	if err != nil {
 		t.Fatalf("createPassword() = %q, want nil", err)
 	}
-	// Default to the 'http' service.
-	os.Args = []string{"", "update", "-n", "-m", expectedMachine, "-u", expectedUser, "-p", "newpassword"}
+	os.Args = []string{"", "update", "-n", "-i", "1", "-p", "newpassword"}
 	inBuf := new(bytes.Buffer)
 	outBuf := new(bytes.Buffer)
 
@@ -306,6 +305,49 @@ func TestUpdateUser(t *testing.T) {
 		t.Fatalf("actualLength = %q, want %q", actualLength, expectedLength)
 	}
 	actualContains := ContainsString(results, fmt.Sprintf("machine: %s, service: %s, user: %s, password type: %s, password: %s", expectedMachine, expectedService, expectedUser, expectedType, "oldpassword"))
+	expectedContains := true
+	if actualContains != expectedContains {
+		t.Fatalf("actualContains = %v, want %v", actualContains, expectedContains)
+	}
+}
+
+func TestUpdateType(t *testing.T) {
+	ctx := CreateContextForTesting(t)
+	expectedMachine := "mymachine"
+	expectedService := "myservice"
+	expectedUser := "myuser"
+	expectedType := "plain"
+	expectedPassword := "mypassword"
+	_, err := createPassword(&ctx, expectedMachine, expectedService, expectedUser, expectedPassword, "oldtype")
+	if err != nil {
+		t.Fatalf("createPassword() = %q, want nil", err)
+	}
+	os.Args = []string{"", "update", "--id", "1", "-t", expectedType}
+	inBuf := new(bytes.Buffer)
+	outBuf := new(bytes.Buffer)
+
+	actualRet := Main(inBuf, outBuf)
+
+	expectedRet := 0
+	if actualRet != expectedRet {
+		t.Fatalf("Main() = %q, want %q", actualRet, expectedRet)
+	}
+	expectedBuf := "Updated 1 password\n"
+	if outBuf.String() != expectedBuf {
+		t.Fatalf("Main() output is %q, want %q", outBuf.String(), expectedBuf)
+	}
+	opts := searchOptions{}
+	opts.noid = true
+	results, err := readPasswords(ctx.Database, opts)
+	if err != nil {
+		t.Fatalf("readPasswords() err = %q, want nil", err)
+	}
+	actualLength := len(results)
+	expectedLength := 1
+	if actualLength != expectedLength {
+		t.Fatalf("actualLength = %q, want %q", actualLength, expectedLength)
+	}
+	actualContains := ContainsString(results, fmt.Sprintf("machine: %s, service: %s, user: %s, password type: %s, password: %s", expectedMachine, expectedService, expectedUser, expectedType, expectedPassword))
 	expectedContains := true
 	if actualContains != expectedContains {
 		t.Fatalf("actualContains = %v, want %v", actualContains, expectedContains)
