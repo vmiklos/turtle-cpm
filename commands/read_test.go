1e490028b650164b50e1d60a50369bb5c343ecd4
Miklos Vajna <vmiklos@vmiklos.hu>
2024-03-30 17:22:23 +0100

    read: clean up TestSelectImplicitFilter()

--- commands/read_test.go
+++ commands/read_test.go
@@ -261,14 +261,13 @@ func TestSelectTypeFilter(t *testing.T) {
 
 func TestSelectImplicitFilter(t *testing.T) {
 	ctx := CreateContextForTesting(t)
-	secure := false
-	_, err := createPassword(&ctx, "mymachine1", "myservice1", "myuser1", "mypassword1", "plain", secure)
+	_, err := ctx.Database.Exec("insert into passwords (machine, service, user, password, type) values('mymachine1', 'myservice1', 'myuser1', 'mypassword1', 'plain')")
 	if err != nil {
-		t.Fatalf("createPassword() = %q, want nil", err)
+		t.Fatalf("db.Exec() = %q, want nil", err)
 	}
-	_, err = createPassword(&ctx, "mymachine2", "myservice2", "myuser2", "mypassword2", "plain", secure)
+	_, err = ctx.Database.Exec("insert into passwords (machine, service, user, password, type) values('mymachine2', 'myservice2', 'myuser2', 'mypassword2', 'plain')")
 	if err != nil {
-		t.Fatalf("createPassword() = %q, want nil", err)
+		t.Fatalf("db.Exec() = %q, want nil", err)
 	}
 	// Implicit search, also not telling that myservice1 is a service.
 	os.Args = []string{"", "--noid", "myservice1"}
