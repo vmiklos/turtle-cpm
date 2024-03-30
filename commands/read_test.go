3c073718a9e961debc15907908a91cc410cb7129
Miklos Vajna <vmiklos@vmiklos.hu>
2024-03-30 17:25:55 +0100

    read: clean up TestSelectUserFilter()

--- commands/read_test.go
+++ commands/read_test.go
@@ -203,14 +203,13 @@ func TestSelectServiceFilter(t *testing.T) {
 
 func TestSelectUserFilter(t *testing.T) {
 	ctx := CreateContextForTesting(t)
-	secure := false
-	_, err := createPassword(&ctx, "mymachine1", "myservice1", "myuser1", "mypassword1", "plain", secure)
+	_, err := ctx.Database.Exec("insert into passwords (machine, service, user, password, type) values('mymachine1', 'myservice1', 'myuser1', 'mypassword1', 'plain')")
 	if err != nil {
-		t.Fatalf("createPassword() = %q, want nil", err)
+		t.Fatalf("db.Exec() = %q, want nil", err)
 	}
-	_, err = createPassword(&ctx, "mymachine2", "myservice2", "myuser2", "mypassword2", "plain", secure)
+	_, err = ctx.Database.Exec("insert into passwords (machine, service, user, password, type) values('mymachine2', 'myservice2', 'myuser2', 'mypassword2', 'plain')")
 	if err != nil {
-		t.Fatalf("createPassword() = %q, want nil", err)
+		t.Fatalf("db.Exec() = %q, want nil", err)
 	}
 	os.Args = []string{"", "search", "--noid", "-u", "myuser1"}
 	inBuf := new(bytes.Buffer)
